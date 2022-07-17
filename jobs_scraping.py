1bbaf0ddf16446cf1221fbec52170ed229f5a05c
mehmettuzcu22 <mehmettuzcu22@gmail.com>
2022-07-17 22:49:45 +0300

    first commit

---
+++ jobs_scraping.py
@@ -0,0 +1,136 @@
+import requests
+import numpy as np
+import pandas as pd
+from datetime import date, datetime, timedelta
+import warnings
+import pandas.io.sql as psql
+import sqlalchemy
+from sqlalchemy import create_engine
+import os
+import argparse
+
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+
+warnings.simplefilter(action='ignore', category=Warning)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+pd.set_option('display.max_rows', None)
+
+
+
+jobs = ['engineer', 'scientist', 'analyst']
+country = ['geoUrn-%3Eurn%3Ali%3Afs_geo%3A102105699,locationFallback-%3ETurkey', 'geoUrn-%3Eurn%3Ali%3Afs_geo%3A103644278,locationFallback-%3EUnited%20States',
+           'geoUrn-%3Eurn%3Ali%3Afs_geo%3A102890719,locationFallback-%3ENetherlands', 'geoUrn-%3Eurn%3Ali%3Afs_geo%3A101282230,locationFallback-%3EGermany',
+           'geoUrn-%3Eurn%3Ali%3Afs_geo%3A105117694,locationFallback-%3ESweden']
+
+
+
+cookies = {
+
+    'JSESSIONID': '"ajax:0872958190215893462"',
+    'li_at': 'AQEDASq5JWoDJalFAAABgfJOvZ8AAAGCFltBn00AVJc-6mNzmn-3QZm4dwHtlPB40vOZqiQJXkCjv-8f-aa3MQ0QOryBotqPTM_3L46A0_P7ADYPkOuqL63pWkwwoh-fO3b898Z7aiRHFWTUPDNj1Sqq',
+}
+
+headers = {
+    'csrf-token': 'ajax:0872958190215893462',
+    'x-restli-protocol-version': '2.0.0',
+}
+
+########### jobPostingId ####################
+
+jobPostingId = []
+company_name= []
+loop_number = 100
+
+for ctr in country:
+  for j in jobs:
+    for i in range(0, loop_number, 25):
+      response = requests.get(f'https://www.linkedin.com/voyager/api/search/hits?decorationId=com.linkedin.voyager.deco.jserp.WebJobSearchHitWithSalary-25&count=25&filters=List(timePostedRange-%3Er86400,distance-%3E25.0,sortBy-%3ER,{ctr}, resultType-%3EJOBS)&keywords=data%20{j}&origin=JOB_SEARCH_PAGE_OTHER_ENTRY&q=jserpFilters&queryContext=List(primaryHitType-%3EJOBS,spellCorrectionEnabled-%3Etrue)&start={i}&skip={i}&topNRequestedFlavors=List(HIDDEN_GEM,IN_NETWORK,SCHOOL_RECRUIT,COMPANY_RECRUIT,SALARY,JOB_SEEKER_QUALIFIED,PRE_SCREENING_QUESTIONS,SKILL_ASSESSMENTS,ACTIVELY_HIRING_COMPANY,TOP_APPLICANT)', cookies=cookies, headers=headers)
+      data = response.json()
+      print(response.status_code)
+
+      for i in range(0,25):
+        try:
+          jobPostingId.append(data['elements'][i]['hitInfo']['com.linkedin.voyager.deco.jserp.WebSearchJobJserpWithSalary']['jobPostingResolutionResult']['jobPostingId'])
+        except:
+          jobPostingId.append(np.NaN)
+
+        try:
+          company_name.append(data['elements'][i]['hitInfo']['com.linkedin.voyager.deco.jserp.WebSearchJobJserpWithSalary']['jobPostingResolutionResult']['companyDetails']['com.linkedin.voyager.deco.jserp.WebJobPostingWithCompanyName']['companyResolutionResult']['name'])
+        except:
+          company_name.append(np.NaN)
+
+
+df = pd.DataFrame({"jobPostingId":jobPostingId,
+                  "company_name": company_name})
+
+len(jobPostingId)
+len(company_name)
+jobPostingId[0:5]
+company_name[0:5]
+
+df = df.loc[df['jobPostingId'].notnull()]
+df['jobPostingId'] = df['jobPostingId'].apply(np.int64)
+
+df.head()
+
+
+#######################################
+
+
+headers = {
+    'accept': 'application/vnd.linkedin.normalized+json+2.1',
+    'csrf-token': 'ajax:0872958190215893462'
+}
+
+detail_data = pd.DataFrame()
+
+for i in df['jobPostingId']:
+  try:
+    response = requests.get(f'https://www.linkedin.com/voyager/api/jobs/jobPostings/{i}', cookies=cookies, headers=headers)
+  except:
+    print("error")
+  data = response.json()
+  # print(data)
+  # print(response.status_code)
+
+  try:
+    dataframe = pd.json_normalize(data['data'])
+    detail_data = detail_data.append(dataframe[['jobPostingId', 'title',  'localizedCostPerApplicantChargeableRegion',  'originalListedAt', 'expireAt', 'createdAt', 'listedAt', 'views', 'applies','formattedLocation', 'jobPostingUrl', 'jobFunctions', 'jobState', 'formattedEmploymentStatus',  'description.text' ]])
+  except:
+    print("Error on:", i)
+
+detail_data["applies"] = detail_data["applies"].astype(int)
+jobs_details = pd.merge(df, detail_data, how="left", on="jobPostingId")
+
+
+def timestamp_convert(dataframe):
+  at_date = [i for i in dataframe.columns if 'At' in i]
+  for col in at_date:
+    dataframe[col] = dataframe[col].apply(lambda d: datetime.utcfromtimestamp(int(d)/1000).strftime('%Y-%m-%d %H:%M:%S'))
+    dataframe[col]= pd.to_datetime(dataframe[col])
+
+  return dataframe
+
+jobs_details2 = jobs_details.loc[jobs_details['createdAt'].notnull()]
+
+
+jobs_details3 = timestamp_convert(jobs_details2)
+
+jobs_details3.info()
+
+jobs_details3.head()
+
+jobs_details3['localizedCostPerApplicantChargeableRegion'].unique()
+
+
+engine = create_engine(f'postgresql://root:root@localhost:5432/test')
+engine.connect()
+
+jobs_details3.head(n=0).to_sql(name='linkedinJobs', con=engine, if_exists='replace')
+jobs_details3.to_sql(name='linkedinJobs', con=engine, if_exists='append')
+
